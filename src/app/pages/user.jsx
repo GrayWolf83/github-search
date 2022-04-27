ff353e520888a7602ba7bb8f2a91ff8dfbf3a318
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2022-04-27 21:59:13 +0500

    user page

--- src/app/pages/user.jsx
+++ src/app/pages/user.jsx
@@ -1,14 +1,63 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import BackButton from '../components/common/back-button'
+import { getCurrentUser, loadCurrentUser } from '../store/users'
 
 const User = () => {
 	const { login } = useParams()
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		if (login) {
+			dispatch(loadCurrentUser(login))
+		}
+	}, [])
+
+	const user = useSelector(getCurrentUser())
+
+	console.log('user', user)
 
 	return (
 		<>
 			<BackButton />
-			<h1>{login}</h1>
+			{user && (
+				<div className='row mt-2'>
+					<div className='col-md-4'>
+						<img
+							src={user.avatar_url}
+							width={200}
+							className='rounded'
+							alt=''
+						/>
+						<h3 className='fw-bold mt-2'>{user.name}</h3>
+						<h4 className='mt-2'>{user.login}</h4>
+						<p>Подписчики: {user.followers}</p>
+						<p>Подписки: {user.following}</p>
+						<p>Репозиториев: {user.public_repos}</p>
+						<p>
+							<a
+								href={user.html_url}
+								target='_blank'
+								rel='noreferrer'
+								className='text-dark text-decoration-underline'>
+								Профиль на GitHub
+							</a>
+						</p>
+						<p>
+							<a
+								href={user.blog}
+								target='_blank'
+								rel='noreferrer'
+								className='text-dark text-decoration-underline'>
+								Личный блог
+							</a>
+						</p>
+					</div>
+					<div className='col-md-8 col-lg-9'></div>
+				</div>
+			)}
 		</>
 	)
 }
